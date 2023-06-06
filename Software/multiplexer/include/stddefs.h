10aa0227e87843c9461670b810560b5350e7ea33
Jakob <jajaho2@web.de>
2023-06-06 16:44:32 +0200

    Initial commit of mulitplexer software

---
+++ Software/multiplexer/include/stddefs.h
@@ -0,0 +1,23 @@
+#ifndef STDDEF_H_
+#define STDDEF_H_
+
+
+#define __AVR_ATmega328P__
+#define F_CPU 16000000UL
+
+#define BAUD 9600
+
+// Pins //
+
+#define BOARD_LED PB5
+
+// MAKROS //
+
+#define SETBIT(port, pin) (port |= (1 << pin))
+#define CLRBIT(port, pin) (port &= ~(1 << pin))
+#define BITSET(port, pin) (port&(1 << pin))
+
+#define LED_ON() SETBIT(PORTB, BOARD_LED)
+#define LED_OFF() CLRBIT(PORTB, BOARD_LED)
+
+#endif /* STDDEF_H_ */
---
+++ Software/multiplexer/src/main.c
@@ -0,0 +1,17 @@
+#include "stddef.h"
+#include <avr/io.h>
+#include <util/setbaud.h>
+#include <util/delay.h>  
+
+int main(void) {
+
+    DDRB = (1 << PB5);
+    uart_init(); 
+    char string[9] = "";
+    
+    while (1) // main loop
+    {
+        uart_gets(&string, 9);
+        uart_puts(&string);
+    }
+}
