6fd0bf87c5e4d01d7d0bb596f6f915adec8e5b5f
Jakob Holz <jajaho2@web.de>
2023-06-06 17:03:45 +0200

    Fixes include errors

--- Software/multiplexer/src/main.c
+++ Software/multiplexer/src/main.c
@@ -5,13 +5,13 @@
 
 int main(void) {
 
-    DDRB = (1 << PB5);
-    uart_init(); 
-    char string[9] = "";
-    
-    while (1) // main loop
-    {
-        uart_gets(&string, 9);
-        uart_puts(&string);
-    }
+    DDRB = (1 << PB5);
+    uart_init(); 
+    char string[9] = "";
+    
+    while (1) // main loop
+    {
+        uart_gets(&string, 9);
+        uart_puts(&string);
+    }
 }
