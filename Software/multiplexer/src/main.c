9301709eff149fba211c442d89397eec73a2d117
Jakob Holz <jajaho2@web.de>
2023-07-01 18:55:00 +0200

    Worked on uart com with ttester

--- Software/multiplexer/src/main.c
+++ Software/multiplexer/src/main.c
@@ -15,7 +15,7 @@ FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
 
 int main(void) {
     uint8_t i;
-    char buf[20], s[20];
+    char buf[40], s[40];
     char meausurement[8];
 
     // -------- Inits --------- //
@@ -33,9 +33,16 @@ int main(void) {
     SET(PORT, TTESTER_NRST);
 
     mux_init();
+    uart_init(); 
+
+    // The default stdio streams stdin, stdout, and stderr are set up (using the same buffer) by using the existing static FILE stream objects.
+    // allows to use the shorthand functions (e.g. printf() instead of fprintf())
+    stdout = stdin = &uart_str;
+
+    // -------- Main Program -------- //
+
 
     mux_set_channel('D', 1);
-
     mux_enable();
 
     CLR(PORT, TTESTER_NRST);
@@ -47,40 +54,41 @@ int main(void) {
     CLR(PORT, TTESTER_NSTART);
     _delay_ms(300);
     SET(PORT, TTESTER_NSTART);
-    
-    
-    /*
-    uart_init(); 
-    // The default stdio streams stdin, stdout, and stderr are set up by using the existing static FILE stream objects.
-    // allows to use the shorthand functions (e.g. printf() instead of fprintf())
-    stdout = stdin = &uart_str;
 
-    _delay_ms(300);
-    CLR(PORT, TTESTER_START);
-    _delay_ms(300);
-    SET(PORT, TTESTER_START);
-    _delay_ms(1000);
-
-    //printf_P(PSTR("Enter command: "));
-    if (fgets(buf, sizeof buf - 1, stdin) == NULL) {
-                SET(PORT, BOARD_LED);
-                _delay_ms(1000);
-                CLR(PORT, BOARD_LED);                       
-    }  // fgets() reads string into internal buffer, returns NULL on error or when end of file occurs while no characters have been read.
-
-    //if (tolower(buf[0]) == 'q')               // tolower() converts uppercase letters to lowercase
-    //    return 0;
-
-
-    if (sscanf(buf, "[RL]", s) > 0){
-        fgets(meausurement, 8, buf);
-    }
-    */
    while (1)
     {
-        SET(PORT, BOARD_LED);
-        _delay_ms(500);
+        
+        
+        // Reads string into internal buffer, returns NULL on error or when end of file occurs while no characters have been read.
+        if (fgets(buf, sizeof buf - 1, stdin) == NULL) {
+                    SET(PORT, BOARD_LED);
+                    _delay_ms(1000);
+                    CLR(PORT, BOARD_LED);                       
+        }  
+        /*
+        //if (tolower(buf[0]) == 'q')               // tolower() converts uppercase letters to lowercase
+        //    return 0;
+
+        
+        if (sscanf(buf, "O", s) > 0){
+            //fgets(meausurement, 8, buf);
+            SET(PORT, BOARD_LED);
+            _delay_ms(1000);
+        } 
+        else {
+            SET(PORT, BOARD_LED);
+            _delay_ms(200);
+        }
         CLR(PORT, BOARD_LED);
-        _delay_ms(500);
+        _delay_ms(1000);
+        */
+        SET(PORT, BOARD_LED);
+        _delay_ms(200);
+        CLR(PORT, BOARD_LED);
+        _delay_ms(200);
+        SET(PORT, BOARD_LED);
+        _delay_ms(200);
+        CLR(PORT, BOARD_LED);
+        _delay_ms(1000);
     }
 }
